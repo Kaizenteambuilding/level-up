63190c108d5d794e5ad352b2879b9125be7bd29f
Kaizenteambuilding <luislopez@kaizenteambuilding.com>
2026-08-11 14:49:07 +0200

    Add parent setup page

---
+++ app/parent/setup/page.tsx
@@ -0,0 +1,9 @@
+import PlayerSetup from '@/components/PlayerSetup'
+
+export default function ParentSetupPage() {
+  return (
+    <main className="shell">
+      <PlayerSetup />
+    </main>
+  )
+}
