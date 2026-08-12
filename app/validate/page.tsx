0dcee6c5d7e76992dfd8b5c1abc675977c1945f3
Kaizenteambuilding <luislopez@kaizenteambuilding.com>
2026-08-12 20:37:34 +0200

    Add curriculum validator

---
+++ app/validate/page.tsx
@@ -0,0 +1,9 @@
+import CurriculumValidator from '@/components/CurriculumValidator'
+
+export default function ValidatePage() {
+  return (
+    <main className="shell">
+      <CurriculumValidator />
+    </main>
+  )
+}
