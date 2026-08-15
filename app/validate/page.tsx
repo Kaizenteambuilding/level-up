f28e04686be9b45c492998f763235bd9ef4a178e
Kaizenteambuilding <luislopez@kaizenteambuilding.com>
2026-08-15 13:32:05 +0200

    Remove legacy and development-only routes

--- app/validate/page.tsx
+++
@@ -1,9 +0,0 @@
-import CurriculumValidator from '@/components/CurriculumValidator'
-
-export default function ValidatePage() {
-  return (
-    <main className="shell">
-      <CurriculumValidator />
-    </main>
-  )
-}
