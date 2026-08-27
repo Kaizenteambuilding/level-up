8b036e00ae9ca9fd9475c23bf02a149e46159039
Kaizenteambuilding <luislopez@kaizenteambuilding.com>
2026-08-27 22:20:52 +0200

    feat: add Spanish writing replay route

---
+++ app/zone/language/writing/replay/page.tsx
@@ -0,0 +1,5 @@
+import MissionGuard from '@/components/MissionGuard'
+
+export default function SpanishWritingReplayPage() {
+  return <main className="shell game-shell"><MissionGuard mode="spanish_writing_replay" /></main>
+}
