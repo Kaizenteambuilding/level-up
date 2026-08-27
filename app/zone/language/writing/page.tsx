9c71a9831da8da803aac247cd8e05fae7e4e19e0
Kaizenteambuilding <luislopez@kaizenteambuilding.com>
2026-08-27 21:09:55 +0200

    feat: add Spanish writing district route

---
+++ app/zone/language/writing/page.tsx
@@ -0,0 +1,5 @@
+import MissionGuard from '@/components/MissionGuard'
+
+export default function SpanishWritingPage() {
+  return <main className="shell game-shell"><MissionGuard mode="spanish_writing" /></main>
+}
