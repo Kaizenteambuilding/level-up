83e81bc83539a0f5cf37d41e0f1ef178d19647f8
Kaizenteambuilding <luislopez@kaizenteambuilding.com>
2026-08-28 07:48:23 +0200

    fix: surface writing session recovery controls

--- app/zone/language/writing/page.tsx
+++ app/zone/language/writing/page.tsx
@@ -2,7 +2,8 @@ import MissionGuard from '@/components/MissionGuard'
 import SpanishWritingExactRequirements from '@/components/SpanishWritingExactRequirements'
 import SpanishWritingReplayLink from '@/components/SpanishWritingReplayLink'
 import SpanishWritingReplayButton from '@/components/SpanishWritingReplayButton'
+import SpanishWritingStallRecovery from '@/components/SpanishWritingStallRecovery'
 
 export default function SpanishWritingPage() {
-  return <main className="shell game-shell"><SpanishWritingExactRequirements /><SpanishWritingReplayLink /><SpanishWritingReplayButton /><MissionGuard mode="spanish_writing" /></main>
+  return <main className="shell game-shell"><SpanishWritingExactRequirements /><SpanishWritingReplayLink /><SpanishWritingReplayButton /><SpanishWritingStallRecovery /><MissionGuard mode="spanish_writing" /></main>
 }
