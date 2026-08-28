989de193c1e89b01df07b971204f6655775855b8
Kaizenteambuilding <luislopez@kaizenteambuilding.com>
2026-08-28 09:06:25 +0200

    fix: remove recursive writing DOM observers

--- app/zone/language/writing/page.tsx
+++ app/zone/language/writing/page.tsx
@@ -1,9 +1,5 @@
 import MissionGuard from '@/components/MissionGuard'
-import SpanishWritingExactRequirements from '@/components/SpanishWritingExactRequirements'
-import SpanishWritingReplayLink from '@/components/SpanishWritingReplayLink'
-import SpanishWritingReplayButton from '@/components/SpanishWritingReplayButton'
-import SpanishWritingStallRecovery from '@/components/SpanishWritingStallRecovery'
 
 export default function SpanishWritingPage() {
-  return <main className="shell game-shell"><SpanishWritingExactRequirements /><SpanishWritingReplayLink /><SpanishWritingReplayButton /><SpanishWritingStallRecovery /><MissionGuard mode="spanish_writing" /></main>
+  return <main className="shell game-shell"><MissionGuard mode="spanish_writing" /></main>
 }
