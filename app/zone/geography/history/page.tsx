07b5b49cf5c15a6b05d301a57ba18156bcd8c6a1
Kaizenteambuilding <luislopez@kaizenteambuilding.com>
2026-08-29 10:23:12 +0200

    feat: route ancient history archive

---
+++ app/zone/geography/history/page.tsx
@@ -0,0 +1,5 @@
+import MissionGuard from '@/components/MissionGuard'
+
+export default function GeographyHistoryPage() {
+  return <main className="shell game-shell"><MissionGuard mode="history_ancient" /></main>
+}
