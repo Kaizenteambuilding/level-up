2aa1350a32a5f69c5fe3076a678bef64f21a2a0d
Kaizenteambuilding <luislopez@kaizenteambuilding.com>
2026-08-29 08:34:46 +0200

    feat: add cartography route

---
+++ app/zone/geography/maps/page.tsx
@@ -0,0 +1,5 @@
+import MissionGuard from '@/components/MissionGuard'
+
+export default function GeographyMapsPage() {
+  return <main className="shell game-shell"><MissionGuard mode="geography_maps" /></main>
+}
