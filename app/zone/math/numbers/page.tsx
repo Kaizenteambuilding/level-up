e28997263cda22265818d50aa27ada012dd157b0
Kaizenteambuilding <luislopez@kaizenteambuilding.com>
2026-08-30 09:30:34 +0200

    feat: add math numbers district route

---
+++ app/zone/math/numbers/page.tsx
@@ -0,0 +1,2 @@
+import MissionGuard from '@/components/MissionGuard'
+export default function MathNumbersPage(){return <main className="shell game-shell"><MissionGuard mode="math_numbers" /></main>}
