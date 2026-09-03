84e3e36666227ceda3901bbd7d56c5a415e7a2fd
Kaizenteambuilding <luislopez@kaizenteambuilding.com>
2026-09-03 09:54:32 +0200

    style: theme physical geography mission

--- app/zone/geography/physical/page.tsx
+++ app/zone/geography/physical/page.tsx
@@ -1,5 +1,5 @@
 import MissionGuard from '@/components/MissionGuard'
 
 export default function GeographyPhysicalPage() {
-  return <main className="shell game-shell"><MissionGuard mode="geography_physical" /></main>
+  return <main className="shell game-shell gh-console geo-physical"><MissionGuard mode="geography_physical" /></main>
 }
